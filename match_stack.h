121bfa202ccffaee55198bb28983e8309fa98a84
GrahamGoudeau21 <grahamgoudeau@gmail.com>
2014-08-24 17:19:50 -0400

    Create match_stack.h

---
+++ match_stack.h
@@ -0,0 +1,37 @@
+#ifndef MATCH_STACK_H_INCLUDED
+#define MATCH_STACK_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+class Stack {
+public:
+    Stack();
+    ~Stack();
+    void push(char character);
+    char pop();
+    bool stack_empty();
+
+private:
+    char *char_stack;
+    int stack_index;
+
+    void expand();
+};
+
+class int_Stack {
+public:
+    int_Stack();
+    ~int_Stack();
+    void push(int location);
+    int pop();
+    bool stack_empty();
+
+private:
+    int *int_stack;
+    int stack_index;
+
+    void expand();
+};
+
+#endif // MATCH_STACK_H_INCLUDED
