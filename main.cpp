2ad25e4e3211a2f9fc334de868e0a8a7433e441d
Graham Goudeau <grahamgoudeau@gmail.com>
2014-09-05 17:00:48 -0400

    Fixed tabs/spacing issue in master/main

--- main.cpp
+++ main.cpp
@@ -11,32 +11,32 @@ string remove_spaces(string brainfuck);
 
 int main()
 {
-    print_begin_prompt();
+	print_begin_prompt();
 
-    string brainfuck;
-    cout << prompt;
-    getline(cin, brainfuck);
+	string brainfuck;
+	cout << prompt;
+	getline(cin, brainfuck);
 
-    while (brainfuck != "exit") {
-        brainfuck = remove_spaces(brainfuck);
-        Source code = Source(brainfuck);
-        if (code.is_valid() && code.is_matched())
-            code.evaluate();
+	while (brainfuck != "exit") {
+		brainfuck = remove_spaces(brainfuck);
+		Source code = Source(brainfuck);
+		if (code.is_valid() && code.is_matched())
+			code.evaluate();
 
-        cout << prompt;
-        getline(cin, brainfuck);
-    }
-    return 0;
+		cout << prompt;
+		getline(cin, brainfuck);
+	}
+	return 0;
 }
 
 void print_begin_prompt() {
-    cout << "BRAINFUCK INTERPRETER\n";
-    cout << "Type 'exit' to quit\n\n";
+	cout << "BRAINFUCK INTERPRETER\n";
+	cout << "Type 'exit' to quit\n\n";
 }
 
 string remove_spaces(string brainfuck) {
-    brainfuck.erase(remove(brainfuck.begin(),brainfuck.end(),' '),brainfuck.end()); ///YOLO
-    return brainfuck;
+	brainfuck.erase(remove(brainfuck.begin(),brainfuck.end(),' '),brainfuck.end()); ///YOLO
+	return brainfuck;
 }
 
 
