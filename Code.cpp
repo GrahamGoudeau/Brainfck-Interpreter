3eaa3541cd9b3fb76a2688f1db363d74e5c61c26
Graham Goudeau <grahamgoudeau@gmail.com>
2014-09-05 16:29:49 -0400

    Fixed tabs/spacing issues

--- Code.cpp
+++ Code.cpp
@@ -12,23 +12,23 @@ int mod (int a, int b);
 
 /**
  * purp: initialize source class with string parameter, tape head set to beginning of memory,
- *       memory initially set to 10 memory cells, all set to 0
+ *	   memory initially set to 10 memory cells, all set to 0
  */
 Source::Source(string brainfuck) {
-    source_code     = brainfuck;
-    tape_head_loc   = 0;
-    memory_size     = 10;
-    memory_tape     = new int[memory_size];
+	source_code	 = brainfuck;
+	tape_head_loc	 = 0;
+	memory_size	 = 10;
+	memory_tape	 = new int[memory_size];
 
-    for (int i = 0; i < memory_size; i++)
-        memory_tape[i] = 0;
+	for (int i = 0; i < memory_size; i++)
+		memory_tape[i] = 0;
 }
 
 /**
  * purp: ensure that the memory tape storage is properly deleted
  */
 Source::~Source() {
-    delete [] memory_tape;
+	delete [] memory_tape;
 }
 
 /**
@@ -36,30 +36,30 @@ Source::~Source() {
  * note: prints out location of first located invalid command
  */
 bool Source::is_valid() {
-    int str_len = source_code.length();
+	int str_len = source_code.length();
 
-    char valid_symbols[8] = {'<', '>', '+', '-', '[', ']', '.', ','};
-    for (int i = 0; i < str_len; i++) {
-        if (!in(source_code[i], valid_symbols)) {
-            cout << "INVALID COMMAND AT " << i + 1 << ": '" << source_code[i] << "'" << endl;
-            return false;
-        }
-    }
+	char valid_symbols[8] = {'<', '>', '+', '-', '[', ']', '.', ','};
+	for (int i = 0; i < str_len; i++) {
+		if (!in(source_code[i], valid_symbols)) {
+			cout << "INVALID COMMAND AT " << i + 1 << ": '" << source_code[i] << "'" << endl;
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 
 /**
  * purp: check to see if char parameter is included in the list of valid symbols
- *       returns true at first valid character that matches the given parameter
+ *	   returns true at first valid character that matches the given parameter
  */
 bool Source::in(char character, char valid_symbols[]) {
-    /// 8 valid characters to check
-    for (int i = 0; i < 8; i++)
-        if (valid_symbols[i] == character)
-            return true;
+	/// 8 valid characters to check
+	for (int i = 0; i < 8; i++)
+		if (valid_symbols[i] == character)
+			return true;
 
-    return false;
+	return false;
 }
 
 /**
@@ -67,29 +67,29 @@ bool Source::in(char character, char valid_symbols[]) {
  * note: utilizes char_stack class in match_stack.h
  */
 bool Source::is_matched() {
-    Stack char_stack;
-    int str_len = source_code.length();
+	Stack char_stack;
+	int str_len = source_code.length();
 
-    for (int i = 0; i < str_len; i++) {
-        if (source_code[i] == '[')
-            /// if open bracket, push the bracket onto the stack
-            char_stack.push(source_code[i]);
-        else if (source_code[i] == ']' && !char_stack.stack_empty())
-            /// if open brackets are on the stack, pop one off (pop off matching open bracket)
-            char_stack.pop();
-        else if (source_code[i] == ']' && char_stack.stack_empty()) {
-            /// if close bracket but empty stack, indicates no matching open bracket
-            cout << "IMPROPERLY MATCHED BRACKETS" << endl;
-            return false;
-        }
-    }
+	for (int i = 0; i < str_len; i++) {
+		if (source_code[i] == '[')
+			/// if open bracket, push the bracket onto the stack
+			char_stack.push(source_code[i]);
+		else if (source_code[i] == ']' && !char_stack.stack_empty())
+			/// if open brackets are on the stack, pop one off (pop off matching open bracket)
+			char_stack.pop();
+		else if (source_code[i] == ']' && char_stack.stack_empty()) {
+			/// if close bracket but empty stack, indicates no matching open bracket
+			cout << "IMPROPERLY MATCHED BRACKETS" << endl;
+			return false;
+		}
+	}
 
-    if (!char_stack.stack_empty()) {
-        /// if stack is not empty by the end of the code, indicates hanging open bracket
-        cout << "IMPROPERLY MATCHED BRACKETS" << endl;
-        return false;
-    }
-    return true;
+	if (!char_stack.stack_empty()) {
+		/// if stack is not empty by the end of the code, indicates hanging open bracket
+		cout << "IMPROPERLY MATCHED BRACKETS" << endl;
+		return false;
+	}
+	return true;
 }
 
 /// download more RAM here
@@ -97,109 +97,109 @@ bool Source::is_matched() {
  * purp: expand the dynamic array containing the memory tape
  */
 void Source::expand() {
-    int new_memory_size = memory_size * 2 + 1;
-    int *downloaded_RAM = new int[new_memory_size];
+	int new_memory_size = memory_size * 2 + 1;
+	int *downloaded_RAM = new int[new_memory_size];
 
-    /// copy old memory values into new expanded array
-    for (int i = 0; i < new_memory_size; i++)
-        if (i < memory_size)
-            downloaded_RAM[i] = memory_tape[i];
-        else
-            downloaded_RAM[i] = 0;
+	/// copy old memory values into new expanded array
+	for (int i = 0; i < new_memory_size; i++)
+		if (i < memory_size)
+			downloaded_RAM[i] = memory_tape[i];
+		else
+			downloaded_RAM[i] = 0;
 
-    /// update memory to increased size, update pointer to memory
-    memory_size = new_memory_size;
-    memory_tape = downloaded_RAM;
+	/// update memory to increased size, update pointer to memory
+	memory_size = new_memory_size;
+	memory_tape = downloaded_RAM;
 }
 
 /****************************************************
-*                   EVALUATE                        *
+*				   EVALUATE						*
 ****************************************************/
 
 /**
  * purp: code valuation manager
  */
 void Source::evaluate() {
-    int_Stack loop_loc_stack;
+	int_Stack loop_loc_stack;
 
-    string output_string = "";
-    int source_code_length = source_code.length();
+	string output_string = "";
+	int source_code_length = source_code.length();
 
-    for (int index = 0; index < source_code_length; index++) {
-        char command = source_code[index];
+	for (int index = 0; index < source_code_length; index++) {
+		char command = source_code[index];
 
-        if (command == '+')
-            memory_tape[tape_head_loc] = mod(memory_tape[tape_head_loc] + 1, 256);
-        else if (command == '-')
-            memory_tape[tape_head_loc] = mod(memory_tape[tape_head_loc] - 1, 256);
+		if (command == '+')
+			memory_tape[tape_head_loc] = mod(memory_tape[tape_head_loc] + 1, 256);
+		else if (command == '-')
+			memory_tape[tape_head_loc] = mod(memory_tape[tape_head_loc] - 1, 256);
 
-        else if (command == '>') {
-            if (tape_head_loc + 1 == memory_size)
-                expand();
-            tape_head_loc++;
-        }
-        else if (command == '<')
-            if (tape_head_loc - 1 < 0) {
-                cout << "ERROR: INVALID MEMORY INDEX ACCESS\n";
-                return;
-            }
-            else
-                tape_head_loc--;
+		else if (command == '>') {
+			if (tape_head_loc + 1 == memory_size)
+				expand();
+			tape_head_loc++;
+		}
+		else if (command == '<')
+			if (tape_head_loc - 1 < 0) {
+				cout << "ERROR: INVALID MEMORY INDEX ACCESS\n";
+				return;
+			}
+			else
+				tape_head_loc--;
 
-        else if (command == '[') {
-            if (memory_tape[tape_head_loc] != 0) {
-		loop_loc_stack.push(index);        
-            }
-            else
-                while (source_code[index] != ']')
-                    index++;
-        }
-        else if (command == ']' && memory_tape[tape_head_loc] != 0) {
-	    index = loop_loc_stack.pop();
-	    loop_loc_stack.push(index);	
+		else if (command == '[') {
+			if (memory_tape[tape_head_loc] != 0) {
+		loop_loc_stack.push(index);		
+			}
+			else
+				while (source_code[index] != ']')
+					index++;
+		}
+		else if (command == ']' && memory_tape[tape_head_loc] != 0) {
+		index = loop_loc_stack.pop();
+		loop_loc_stack.push(index);	
 	}
-        else if (command == ']' && memory_tape[tape_head_loc] == 0)
-	    loop_loc_stack.pop();
-        else if (command == '.') {
-            char output_letter = memory_tape[tape_head_loc];
-            output_string += output_letter;
-        }
-        else if (command == ',') {
-            string input;
-            getline(cin, input);
-            if (input == "\0")
-                memory_tape[tape_head_loc] = 0;
-            else
-                memory_tape[tape_head_loc] = input[0];
-        }
+		else if (command == ']' && memory_tape[tape_head_loc] == 0)
+		loop_loc_stack.pop();
+		else if (command == '.') {
+			char output_letter = memory_tape[tape_head_loc];
+			output_string += output_letter;
+		}
+		else if (command == ',') {
+			string input;
+			getline(cin, input);
+			if (input == "\0")
+				memory_tape[tape_head_loc] = 0;
+			else
+				memory_tape[tape_head_loc] = input[0];
+		}
 
-        /**for (int i = 0; i < memory_size; i++)
-            cout << memory_tape[i] << " ";
-        cout << endl;
+		/**for (int i = 0; i < memory_size; i++)
+			cout << memory_tape[i] << " ";
+		cout << endl;
 
-        for (int i = 0; i < source_code_length; i++) {
-            if (i == index) cout << "*";
-            cout << source_code[i] << " ";
-        }
-        cout << endl << endl;
+		for (int i = 0; i < source_code_length; i++) {
+			if (i == index) cout << "*";
+			cout << source_code[i] << " ";
+		}
+		cout << endl << endl;
 
-        string wait;
-        getline(cin, wait);*/
-    }
+		string wait;
+		getline(cin, wait);*/
+	}
 
-    for (int i = 0; i < memory_size; i++)
-        cout << memory_tape[i] << " ";
-    cout << endl;
+	for (int i = 0; i < memory_size; i++)
+		cout << memory_tape[i] << " ";
+	cout << endl;
 
-    if (output_string.length())
-        cout << output_string << endl;
+	if (output_string.length())
+		cout << output_string << endl;
 
 }
 
 int mod (int a, int b) {
    int result = a % b;
    if(result < 0)
-     result += b;
-     
+	 result += b;
+	 
    return result;
 }
