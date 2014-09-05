bc62edb98917a0ce0f711a2911ccd4a18ee52ce6
Graham Goudeau <grahamgoudeau@gmail.com>
2014-09-05 16:30:59 -0400

    Fixed tabs/spacing issues

--- Code.h
+++ Code.h
@@ -8,21 +8,21 @@ using namespace std;
 class Source {
 
 public:
-    Source(string brainfuck);
-    ~Source();
+	Source(string brainfuck);
+	~Source();
 
-    bool is_valid();
-    bool is_matched();
-    void evaluate();
+	bool is_valid();
+	bool is_matched();
+	void evaluate();
 
 private:
-    string source_code;
-    int tape_head_loc;
-    int memory_size;
-    int *memory_tape;
+	string source_code;
+	int tape_head_loc;
+	int memory_size;
+	int *memory_tape;
 
-    bool in(char character, char valid_symbols[]);
-    void expand();
+	bool in(char character, char valid_symbols[]);
+	void expand();
 };
 
 #endif // CODE_H_INCLUDED
